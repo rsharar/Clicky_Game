991a4b623fbbbd250b6a36424fcfad181080c62f
Rudy Sharar <rudy@activelylearn.com>
2019-01-03 14:23:03 -0800

    added footer and navbar components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+    return (
+        <div>
+            <h1>This is the Footer</h1>
+        </div>
+    )
+}
+
+
+export default Footer
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./style.css";
 
-function Navbar(props){
-    
+function Navbar(props) {
+    return (
+        <div>
+            <h1>This is the Navbar</h1>
+        </div>
+    )
 }
+
+
+export default Navbar
