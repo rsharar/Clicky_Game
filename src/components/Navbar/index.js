fc2f0f02d9d190c8703e86a7b57c9b8b2bbbe062
Rudy Sharar <rudy@activelylearn.com>
2019-01-03 15:04:16 -0800

    added characters.jsonarray and some imgs

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,20 +4,30 @@ import "./style.css";
 
 const NavBar = (props) => {
     return (
-            <Navbar>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href="#home">Memeory</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Navbar.Text>
-                        Click the memes below and don't click the same one twice!
-            </Navbar.Text>
-                    <Navbar.Text pullRight>Score: {props.score} | Top Score: {props.topScore}</Navbar.Text>
-                </Navbar.Collapse>
-            </Navbar>
+
+        <ul className="nav sticky-top justify-content-between align-items-center flex-lg-row flex-column px-3">
+            <li className="nav-item">
+                <img src={`${process.env.PUBLIC_URL}/images/my-hero-logo.png`} id="nav-img" alt="My Hero Academia" />
+            </li>
+            <li className="nav-item text-white h3">
+                {/* {message} */}
+            </li>
+            <li className="nav-item text-white h4">Score: {props.score} | Top Score: {props.topScore}</li>
+        </ul>
+        // <Navbar>
+        //     <Navbar.Header>
+        //         <Navbar.Brand>
+        //             <a href="#home">Memeory</a>
+        //         </Navbar.Brand>
+        //         <Navbar.Toggle />
+        //     </Navbar.Header>
+        //     <Navbar.Collapse>
+        //         <Navbar.Text>
+        //             Click the memes below and don't click the same one twice!
+        //     </Navbar.Text>
+        //         <Navbar.Text pullRight>Score: {props.score} | Top Score: {props.topScore}</Navbar.Text>
+        //     </Navbar.Collapse>
+        // </Navbar>
     )
 }
 export default NavBar;
