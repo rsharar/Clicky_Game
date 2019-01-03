656cdb72c85da421a9b9d677265826b30d78f55c
Rudy Sharar <rudy@activelylearn.com>
2019-01-03 14:48:57 -0800

    react bootstrap navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Navbar } from 'react-bootstrap'
 import "./style.css";
 
-function Navbar(props) {
+const NavBar = (props) => {
     return (
-        <div>
-            <h1>This is the Navbar</h1>
-        </div>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#home">Memeory</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Navbar.Text>
+                        Click the memes below and don't click the same one twice!
+            </Navbar.Text>
+                    <Navbar.Text pullRight>Score: {props.score} | Top Score: {props.topScore}</Navbar.Text>
+                </Navbar.Collapse>
+            </Navbar>
     )
 }
-
-
-export default Navbar
+export default NavBar;
