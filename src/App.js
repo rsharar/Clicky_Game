3fce588413e42a8dcce073a85e3cde21c22c53a6
Rudy Sharar <rudy@activelylearn.com>
2019-01-04 22:07:37 -0800

    images load but not in grid

--- src/App.js
+++ src/App.js
@@ -2,25 +2,29 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import ImageCard from './components/ImageCard';
+// import Wrapper from './components/Wrapper';
 import characters from "./characters.json";
 
 
 import './App.css';
 
 class App extends Component {
+
+  // add all functions for gameplay
+
   render() {
     return (
       <div>
         <div>
           <Navbar />
         </div>
-        <div>
-        {characters.map((character,i)=>{
-        return (
-          <ImageCard characters={character} key={character.id} />
-        )
-      })}
-        </div>
+          <div>
+            {characters.map((character, i) => {
+              return (
+                <ImageCard characters={character} key={character.id} />
+              )
+            })}
+          </div>
         <div>
           <Footer />
         </div>
