9c29f6bf9b5f2ec93862b7d8c646c283c268bf3e
Rudy Sharar <rudy@activelylearn.com>
2019-01-09 20:18:48 -0800

    Updates

---
+++ precache-manifest.b80c979cdac4c704718195e35700db34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ff56a88f59549d49d1c",
+    "url": "/Clicky_Game/static/js/runtime~main.9ff56a88.js"
+  },
+  {
+    "revision": "685fb589bad08c55f958",
+    "url": "/Clicky_Game/static/js/main.685fb589.chunk.js"
+  },
+  {
+    "revision": "b05f58ae460c5f47cea3",
+    "url": "/Clicky_Game/static/js/1.b05f58ae.chunk.js"
+  },
+  {
+    "revision": "685fb589bad08c55f958",
+    "url": "/Clicky_Game/static/css/main.0eddaa56.chunk.css"
+  },
+  {
+    "revision": "7415235b276faeeb41882ea0089c95d3",
+    "url": "/Clicky_Game/index.html"
+  }
+];
